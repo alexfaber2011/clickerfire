76c9af2df078fc8eb57e42dd26da5a12a2184fa0
Alex Faber <alexfaber2011@gmail.com>
2014-03-01 00:26:14 -0600

    create event basically done

--- modules/fapi.js
+++ modules/fapi.js
@@ -1,12 +1,27 @@
 var Firebase = require('firebase');
 
+
 //, startTime, endTime, numWeeks, days
 exports.createEvent = function(eventName){
-	//Find existing event by name
 	var dataRef = new Firebase('https://resplendent-fire-2962.firebaseio.com/class/' + eventName);
-	dataRef.once('value', function(snapshot) {
-	  console.log('returned class ' + snapshot.val().name.value);
+	//Find existing event by name
+	dataRef.once('value', function(snapshot){
+		//console.log(snapshot);
+		if(snapshot.val() === null){
+			console.log("calling insert");
+			insertEvent(eventName, dataRef);
+		}
+		else{
+			console.log('returned class ' + snapshot.val().score);		
+		}
 	});
 
 	//Create if not exist
+	function insertEvent(dataRef){
+		dataRef.set({score: -1}, function(err){
+			console.log("error setting the event");
+		});
+		console.log("inserted");
+	}
 }
+
